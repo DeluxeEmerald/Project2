f6b39ad09830bb1d60e5917fe23e60d7988345a3
JaimeTC0 <ja386078@ucf.edu>
2026-07-14 13:50:31 -0400

    Filtering Works :)

--- frontend/src/components/Inventory.tsx
+++ frontend/src/components/Inventory.tsx
@@ -1,5 +1,7 @@
 import { Container } from 'postcss';
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+
+
 function Inventory()
 {
 
@@ -8,8 +10,10 @@ function Inventory()
     let userId : string = ud.id;
     const [searchResults,setResults] = useState('');
     const [search,setSearchValue] = React.useState('');
-    let isTrueSort = false;
-    let isFilterOptions = false;
+    const isTrueSortRef = useRef(false);
+    const isFilterOptionsRef = useRef(false);
+
+    const typeMap = new Map();
 
     const app_name = 'cop4331-89.xyz';
     function buildPath(route: string): string
@@ -47,9 +51,12 @@ function Inventory()
                 _results.forEach((element: any) => {
                     const cardAdd = document.createElement('div');
                     cardAdd.className = 'card';
-                    cardAdd.innerHTML = `
-                        <img src="${element.imageUrl}" alt="${element.name}" class='h-60 w-40 rounded-md'>`;
-                    container.appendChild(cardAdd);
+                    if(checkFilter(element.typeLine, element.rarity)){
+                        cardAdd.innerHTML = `
+                            <img src="${element.imageUrl}" alt="${element.name}" class='h-60 w-40 rounded-md'>`;
+                        container.appendChild(cardAdd);
+                    }
+                    typeMap.set(element.name, element.typeLine);
                 });
             }
         }
@@ -60,15 +67,35 @@ function Inventory()
         }
     };
 
-    function handleSearchTextChange( e: any ) : void
-    {
+    function handleSearchTextChange( e: any ) : void{
         setSearchValue( e.target.value );
+    }
+
+    function checkFilter(typeLine: string,rarity: string): boolean{
+        const typeId = ['creature', 'land', 'instant', 'sorcery', 'artifact', 'enchantment', 'planeswalker']
+        const rarityId = ['common', 'uncommon', 'rare', 'mythic']
+
+        
+        const selectedTypes = typeId.filter(id =>
+            (document.getElementById(id) as HTMLInputElement)?.checked
+        );
+        const selectedRarities = rarityId.filter(id =>
+            (document.getElementById(id) as HTMLInputElement)?.checked
+        );
+
+        const typeMatch = selectedTypes.length === 0 ||
+            selectedTypes.some(x => typeLine.toLowerCase().includes(x));
+
+        const rarityMatch = selectedRarities.length === 0 ||
+            selectedRarities.includes(rarity.toLowerCase());
+
+        return typeMatch && rarityMatch;
     }
 
     function showSort(){
         const container = document.getElementById('sortOption');
         if (container){
-            if(!isTrueSort){
+            if(!isTrueSortRef.current){
             container.innerHTML = '';
             container.className ='flex justify-center items-center';
 
@@ -93,78 +120,101 @@ function Inventory()
             </div>
             `;
             container.appendChild(list);
-            isTrueSort = true;
+            isTrueSortRef.current = true;
             }
             else{
                 container.innerHTML = '';
-                isTrueSort = false;
+                isTrueSortRef.current = false;
             }
         }
     }
 
+    function setFilter(e: any){
+        searchCard(e);
+    }
+
     function showFilterOptions(){
         const container = document.getElementById('filterOption');
-        if (container){
-            if(!isFilterOptions){
-                container.className ='flex justify-center items-center';
-            container.innerHTML = '';
+        if (!container) return;
 
-            const list = document.createElement('form');
-            list.className = 'grid grid-cols-2 gap-x-4 gap-y-2 w-56 p-3 rounded-md';
-            list.innerHTML = `
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="creature" class="w-4 h-4">
-                <label for="creature" class="text-sm">Creature</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="land" class="w-4 h-4">
-                <label for="land" class="text-sm">Land</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="instant" class="w-4 h-4">
-                <label for="instant" class="text-sm">Instant</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="sorcery" class="w-4 h-4">
-                <label for="sorcery" class="text-sm">Sorcery</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="artifact" class="w-4 h-4">
-                <label for="artifact" class="text-sm">Artifact</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="enchantment" class="w-4 h-4">
-                <label for="enchantment" class="text-sm">Enchantment</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="planeswalker" class="w-4 h-4">
-                <label for="planeswalker" class="text-sm">Planeswalker</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="common" class="w-4 h-4">
-                <label for="common" class="text-sm">Common</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="uncommon" class="w-4 h-4">
-                <label for="uncommon" class="text-sm">Uncommon</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="rare" class="w-4 h-4">
-                <label for="rare" class="text-sm">Rare</label>
-            </div>
-            <div class="flex items-center gap-1">
-                <input type="checkbox" id="mythic" class="w-4 h-4">
-                <label for="mythic" class="text-sm">Mythic</label>
-            </div>
-            <button type="button" class="col-span-2 mx-auto mt-2 px-4 py-1 bg-white rounded-full">Submit</button>
-            `;
-            container.appendChild(list);
-            isFilterOptions = true;
+        
+        if(!isFilterOptionsRef.current){
+            if(!container.hasChildNodes()){
+                container.className ='flex justify-center items-center';
+
+                const list = document.createElement('div');
+                list.className = 'grid grid-cols-2 gap-x-4 gap-y-2 w-86 p-3 rounded-md';
+                list.innerHTML = `
+                <p class='col-span-2'>Ownership</p>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="owned" value="owned" class="w-4 h-4">
+                    <label for="owned" class="text-sm">Owned</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="notOwned" value="notOwned" class="w-4 h-4">
+                    <label for="notOwned" class="text-sm">Not Owned</label>
+                </div>
+                    <p class='col-span-2 justify-center'>Type</p>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="creature" value="creature" class="w-4 h-4">
+                    <label for="creature" class="text-sm">Creature</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="land" value="land" class="w-4 h-4">
+                    <label for="land" class="text-sm">Land</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="instant" value="instant" class="w-4 h-4">
+                    <label for="instant" class="text-sm">Instant</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="sorcery" value="sorcery" class="w-4 h-4">
+                    <label for="sorcery" class="text-sm">Sorcery</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="artifact" value="aritfact" class="w-4 h-4">
+                    <label for="artifact" class="text-sm">Artifact</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="enchantment" value="enchantment" class="w-4 h-4">
+                    <label for="enchantment" class="text-sm">Enchantment</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="planeswalker" value="planeswalker" class="w-4 h-4">
+                    <label for="planeswalker" class="text-sm">Planeswalker</label>
+                </div>
+                <p class='col-span-2'>Rarity</p>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="common" value="common" class="w-4 h-4">
+                    <label for="common" class="text-sm">Common</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="uncommon" value="uncommon" class="w-4 h-4">
+                    <label for="uncommon" class="text-sm">Uncommon</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="rare" value="rare" class="w-4 h-4">
+                    <label for="rare" class="text-sm">Rare</label>
+                </div>
+                <div class="flex items-center gap-1">
+                    <input type="checkbox" id="mythic" value="mythic" class="w-4 h-4">
+                    <label for="mythic" class="text-sm">Mythic</label>
+                </div>
+                <button type="button" class="col-span-2 mx-auto mt-2 px-4 py-1 bg-white rounded-full">Submit</button>
+                `;
+                container.appendChild(list);
+
+                const submitBtn = list.querySelector('button');
+                submitBtn?.addEventListener('click', setFilter);
+
             }
-            else{
-                container.innerHTML = '';
-                isFilterOptions = false;
-            }
+
+            container.style.display = 'flex';
+            isFilterOptionsRef.current = true;
+        }
+        else{
+            container.style.display = 'none';
+            isFilterOptionsRef.current = false;
         }
     }
 
@@ -193,7 +243,7 @@ function Inventory()
             </div>
         </div>
         <span id="cardSearchResult">{searchResults}</span>
-        <p id="cardList" className='flex flex-wrap gap-2 justify-center' ></p>
+        <p id="cardList" className='flex flex-wrap gap-2 justify-center mb-4' ></p>
     </div>
     );
 }
